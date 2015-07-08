546dee54a535aff0c3a0056b9868d7851a70a810
Katarzyna Kasprzak <kasia.kasprzak18@gmail.com>
2015-07-08 15:24:54 +0200

    JqueryAndBootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
+//= require jquery_nested_form
 //= require turbolinks
 //= require_tree .
-//= require jquery_nested_form
-//= require bootstrap
+
+
